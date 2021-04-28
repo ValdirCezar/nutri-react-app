5f3a1b37f2ac1caa415e6627b1c707b2d655a7c9
Valdir Cezar <valdircezar312@gmail.com>
2021-04-28 10:31:46 -0300

    feat: finished project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import React, { Component } from 'react'
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+import './App.css'
 
 class App extends Component {
 
@@ -11,7 +20,7 @@ class App extends Component {
 
   componentDidMount() {
     const url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
-    
+
     fetch(url)
       .then((resposta) => resposta.json())
       .then((json) => {
@@ -24,7 +33,33 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        
+        {this.state.list.map((item) => {
+          return (
+            <Card className="card">
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  alt="Contemplative Reptile"
+                  height="280"
+                  image={item.capa}
+                  title=""
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {item.titulo}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    {item.subtitulo}
+                    </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button variant="outlined" color="primary">Share</Button>
+                <Button variant="outlined" color="primary">Learn More</Button>
+              </CardActions>
+            </Card>
+          );
+        })}
       </div>
     );
   }
